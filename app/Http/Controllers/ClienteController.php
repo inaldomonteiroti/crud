d79e9787cbff337155525b45daa4d7b9887507a9
Inaldo Monteiro <inaldomonteiro@localhost.localdomain>
2020-09-23 14:15:37 -0300

    Listando Clientes

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ClienteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $clientes = \App\Cliente::all();
+        
+        return view('cliente.index', compact('clientes'));
+    }
+
+
+}
--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -5,10 +5,14 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Lista de Clientes</div>
 
                 <div class="panel-body">
-                    Você está logado!
+                    View da Lista de Clientes!
+                @foreach($clientes as $cliente)
+                <li>{{$cliente->name}}</li>
+                @endforeach
+
                 </div>
             </div>
         </div>
