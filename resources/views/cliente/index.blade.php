892330579ae2ca8e594ad8a3939813cfbebfe79f
Inaldo Monteiro <inaldomonteiro@localhost.localdomain>
2020-09-23 14:38:03 -0300

    Tabela de apresentação dos clientes

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -8,12 +8,39 @@
                 <div class="panel-heading">Lista de Clientes</div>
 
                 <div class="panel-body">
-                    View da Lista de Clientes!
-                @foreach($clientes as $cliente)
-                <li>{{$cliente->name}}</li>
-                @endforeach
-                <div align="center">
-                {!! $clientes->links() !!}
+                
+                <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nome</th>
+                                <th>E-mail</th>
+                                <th>Endereço</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($clientes as $cliente)
+
+                            <tr>
+                                <th scope="row">{{ $cliente->id }}</th>
+                                <td>{{ $cliente->name }}</td>
+                                <td>{{ $cliente->email }}</td>
+                                <td>{{ $cliente->endereco }}</td>
+                                <td>
+                                    
+                                </td>
+                            </tr>                            
+
+                            @endforeach
+                            
+                        </tbody>
+                        
+                    </table>
+
+                    <div align="center">
+                        {!! $clientes->links() !!}
+                    </div>
                 </div>
 
 
